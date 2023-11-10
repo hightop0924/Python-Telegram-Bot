c4d0be26f2c76e8478ea74c7d8152de7a16ca316
hightop0924 <hightop.dev.hk@gmail.com>
2023-11-11 06:27:09 +0900

    "modified on 2023-11-11"

--- init_session.py
+++ init_session.py
@@ -13,3 +13,15 @@ accounts = config['accounts']
 folder_session = 'session/'
 
 for account in accounts:
+	api_id = account['api_id']
+	api_hash = account['api_hash']
+	phone = account['phone']
+	print(phone)
+
+	client = TelegramClient(folder_session + phone, api_id, api_hash)
+	client.start()
+	if client.is_user_authorized():
+		print('Login success')
+	else:
+		print('Login fail')
+	client.disconnect()
